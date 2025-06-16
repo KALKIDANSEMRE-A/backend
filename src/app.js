47216458d576cfe8d28b692b9322be33ef189af5
KALKDANSEMRE-A <semrekalkidan@gmail.com>
2025-06-16 14:16:12 +0300

    Fix CORS to allow Vercel frontend

--- src/app.js
+++ src/app.js
@@ -30,7 +30,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://frontend-new-delta.vercel.app",
+
     credentials: true,
   })
 );
